515dd751269d4986955aa2c66b1bec63cbce388a
Voicu Leonard <leonard.voicu@yahoo.com>
2018-04-01 16:29:00 +0300

    L6 - https://egghead.io/courses/start-learning-react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,11 +4,10 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      txt: 'this is the state text',
-      cat: 0
+      txt: 'this is the state text'
     }
   }
-  
+
   update(e){
     this.setState({txt: e.target.value})
   }
@@ -16,11 +15,16 @@ class App extends React.Component{
   render(){
     return(
       <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <h1> {this.state.txt} - {this.state.cat}</h1>
+        <h1> {this.state.txt}</h1>
+        <Widget update={this.update.bind(this)} />
+        <Widget update={this.update.bind(this)} />
+        <Widget update={this.update.bind(this)} />
       </div>
     )
   }
 }
 
+const Widget = (props) =>
+  <input type="text" onChange={props.update}/>
+  
 export default App
