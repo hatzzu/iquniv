496f4a770d825e16295a31693c124a3fe873e9d7
Voicu Leonard <leonard.voicu@yahoo.com>
2018-04-01 15:50:42 +0300

    L2 - https://egghead.io/courses/start-learning-react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 class App extends React.Component{
   render(){
-    return  <h1>Hello</h1>
+    return  <h1>Hello Series</h1>
+    //return React.createElement('h1',null,'Hello Eggheads')
   }
 }
+
+// const App = () => <h1>Hello stateless</h1>
 
 export default App
