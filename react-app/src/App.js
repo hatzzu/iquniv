d4617ac2788efb5908104f0b53f210415aeec02a
Voicu Leonard <leonard.voicu@yahoo.com>
2018-04-01 16:35:30 +0300

    L7 - https://egghead.io/courses/start-learning-react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,30 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      txt: 'this is the state text'
-    }
-  }
-
-  update(e){
-    this.setState({txt: e.target.value})
-  }
-
   render(){
-    return(
-      <div>
-        <h1> {this.state.txt}</h1>
-        <Widget update={this.update.bind(this)} />
-        <Widget update={this.update.bind(this)} />
-        <Widget update={this.update.bind(this)} />
-      </div>
-    )
+    return <Button>I <Heart/> React</Button>
+  }
+}
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component{
+  render(){
+    return <span>&hearts;</span>
   }
 }
 
-const Widget = (props) =>
-  <input type="text" onChange={props.update}/>
-  
 export default App
