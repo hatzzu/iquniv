f19fb4696c3635762c22f8e1121607258632c204
Voicu Leonard <leonard.voicu@yahoo.com>
2018-04-01 16:42:51 +0300

    L8 - https://egghead.io/courses/start-learning-react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 class App extends React.Component{
   render(){
-    return <Button>I <Heart/> React</Button>
+    return <Title text="12345678 "/>
   }
 }
-const Button = (props) => <button>{props.children}</button>
 
-class Heart extends React.Component{
-  render(){
-    return <span>&hearts;</span>
+const Title = (props) => <h1>Title: {props.text}</h1>
+
+Title.propTypes = {
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length<6){
+      return new Error(`${propName} was too short`)
+    }
   }
 }
 
